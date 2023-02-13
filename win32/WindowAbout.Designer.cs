dbabee0f4d6c6f348d93c870d7040d6be81783c3
ganeshh123 <43777439+ganeshh123@users.noreply.github.com>
2023-02-13 23:04:29 +0000

    feat: dynamic app version in about window

--- win32/WindowAbout.Designer.cs
+++ win32/WindowAbout.Designer.cs
@@ -56,9 +56,8 @@
             this.LabelAppVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LabelAppVersion.Location = new System.Drawing.Point(129, 108);
             this.LabelAppVersion.Name = "LabelAppVersion";
-            this.LabelAppVersion.Size = new System.Drawing.Size(44, 20);
+            this.LabelAppVersion.Size = new System.Drawing.Size(0, 20);
             this.LabelAppVersion.TabIndex = 1;
-            this.LabelAppVersion.Text = "1.0.0";
             this.LabelAppVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // pictureBox1
